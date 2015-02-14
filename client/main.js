24e87225fe19f965276899048c7050700e63f6a2
cnichols <mellowdious@mail.com>
2015-02-14 00:10:06 -0800

    WIP added assets and structure

--- client/main.js
+++ client/main.js
@@ -1,1 +1,52 @@
-console.log('hello, world');
+/* global _, Tile */
+
+'use strict';
+
+$(document).ready(init);
+
+var NUMROWS = 4;
+var NUMCOLS = 5;
+var NUMTILES = NUMROWS * NUMCOLS;
+
+var tiles = [];
+
+function init() {
+  createTiles();
+  drawTiles();
+}
+
+function drawTiles() {
+  for (var y = 0; y < NUMROWS; y++) {
+
+    var $row = $('<div>');
+    $row.addClass('row');
+    $row.attr('id', 'row' + y);
+    $('#gamecontainer').append($row);
+
+    for (var x = 0; x < NUMCOLS; x++) {
+      var tile = tiles.pop();
+      var $square = $('<div>');
+      $square.css('background-image', 'url("' + tile.image + '")');
+      $square.addClass('square');
+      $square.attr('id', 'col' + x);
+      $('#row' + y).append($square);
+    }
+
+  }
+  // debugger;
+}
+
+function createTiles() {
+  var images = [];
+  for (var i = 1; i < 25; i++) {
+    images.push('/images/' + i + '.png');
+  }
+  images = _.shuffle(images);
+
+  for (var j = 0; j < NUMTILES / 2; i++) {
+    var tile = new Tile(images.pop());
+    tiles.push(tile);
+    tiles.push(tile);
+  }
+  tiles = _.shuffle(tiles);
+}
---
+++ client/models/tile.js
@@ -0,0 +1,9 @@
+/* global _ */
+/* exported Tile */
+
+'use strict';
+
+var Tile = function(image) {
+  this.image = image;
+  this.back = '/images/back.png';
+};
